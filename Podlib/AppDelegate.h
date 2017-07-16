dd3e783c066af17dfcc081f744e226caceb553a8
wuwentao <wuwentao@qiyi.com>
2017-07-16 14:32:10 +0800

    GIT INIT

---
+++ Podlib/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Podlib
+//
+//  Created by wuwentao on 17/7/16.
+//  Copyright © 2017年 wuwentao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Podlib/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Podlib
+//
+//  Created by wuwentao on 17/7/16.
+//  Copyright © 2017年 wuwentao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
