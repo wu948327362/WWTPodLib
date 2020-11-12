d47d723661109c21bb641f07dff40b86d9c0f62a
wuwentao <wuwentao03@baidu.com>
2020-11-12 16:49:31 +0800

    test

--- Podlib/WWTControlView.h
+++ Podlib/WWTControlView.h
@@ -11,5 +11,5 @@
 @interface WWTControlView : UIView
 @property(nonatomic,copy)NSString *name;
 @property(nonatomic,strong)NSString *branchName;
-@property(nonatomic,strong)NSString *branchName1;
+@property(nonatomic,strong)NSString *branchName12;
 @end
