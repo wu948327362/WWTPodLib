6bb26b52a9df5c34a220daf855490ad03338909f
wuwentao03 <wuwentao03@baidu.com>
2021-07-06 19:01:12 +0800

    test commit

--- Podlib/WWTControlView.m
+++ Podlib/WWTControlView.m
@@ -10,12 +10,8 @@
 
 @implementation WWTControlView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (void)test{
+    
 }
-*/
 
 @end
